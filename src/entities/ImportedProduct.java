53d6cddb163e6482c9e482ac99de425630d3e6d2
coradi2000 <coradi_2000@hotmail.com>
2020-09-23 13:58:19 -0300

    Exercicio pronto.

---
+++ src/entities/ImportedProduct.java
@@ -0,0 +1,32 @@
+package entities;
+
+public class ImportedProduct extends Product {
+
+	private Double customsFee;
+	
+	public ImportedProduct() {
+	}
+
+	public ImportedProduct(String name, Double price, Double customsFee) {
+		super(name, price);
+		this.customsFee = customsFee;
+	}
+
+	public Double getCustomsFee() {
+		return customsFee;
+	}
+
+	public void setCustomsFee(Double customsFee) {
+		this.customsFee = customsFee;
+	}
+	
+	public double totalPrice() {
+		return this.getPrice() + customsFee;
+	}
+	
+	@Override
+	public String priceTag() {
+		return this.getName() + " $ " +  String.format("%.2f",  this.getPrice()) + 
+				" (Customs fee: $ " + String.format("%.2f)", customsFee);
+	}
+}
